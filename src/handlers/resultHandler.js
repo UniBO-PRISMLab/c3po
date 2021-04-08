4412d13c80e6e10b751a01829b08c4174cd5f4ac
Ivan Zyrianoff <ivan.zyrianoff@ufabc.edu.br>
2021-04-08 20:38:06 -0300

    style(result handler): adding logs to errors
    adding complete logs when errors occur when using 'debug' log level

--- src/handlers/resultHandler.js
+++ src/handlers/resultHandler.js
@@ -1,19 +1,23 @@
 const util = require('util');
+const logger = require('../logger');
 
 const errorHandler = (error) => {
   if (error.response) {
+    logger.debug(error.response);
     return Promise.reject(`Request made and server responded
       \ndata: ${util.inspect(error.response.data)}
       \nstatus: ${util.inspect(error.response.status)}
       \nheaders:${util.inspect(error.response.headers)}
       \nURL: ${util.inspect(error.response.config.url)}`);
   } else if (error.request) {
+    logger.debug(error.request);
     return Promise.reject(
       new Error(`The request was made but no response was received `
         // \n${util.inspect(error.request)}
       )
     );
   } else {
+    logger.debug(error);
     return Promise.reject(
       new Error(
         `Something happened in setting up the request that triggered an Error\n${util.inspect(
