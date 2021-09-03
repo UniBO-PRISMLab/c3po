b904c59d38865ff8fd3b2263a54810a3e115e727
ivanzy <idimitryz@gmail.com>
2021-09-03 15:36:29 -0300

    feat: project change to a translator of OAS to WoT Thing Description

--- src/hasher.js
+++
@@ -1,4 +0,0 @@
-const hash = require('object-hash');
-
-module.exports = (thing) =>
-    hash(JSON.stringify(thing), { algorithm: 'md5', encoding: 'base64' });
--- src/routes/management.js
+++ src/routes/management.js
@@ -1,12 +1,10 @@
-const arrowHeadMetadata = require('../poolingMetadata/arrowHeadMetadata.js');
-const modronMetadata = require('../poolingMetadata/modronMetadata.js');
+
 
 module.exports = (router) => {
     router.route("/").get((req, res) => {
         try {
             res.json({
-                modron: modronMetadata.get(),
-                arrowHead: arrowHeadMetadata.get()
+               todo:"add meaningful metadata",
             });
         } catch (err) {
             res.status(503).send();
