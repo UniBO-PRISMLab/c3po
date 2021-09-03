3df27a4c14cfc8857f3d5da47bbdc27c6c993e0a
ivan <idimitryz@gmail.com>
2021-09-03 12:33:30 -0300

    fix(repositories): parametrized IP address to get openAPI description
    Previous implementation hardcoded the openAPI IP for testing purposes. Now, it get the IP from the
    arrowhead service - idk if works, need to test it.

--- src/repositories/openApiRequest.js
+++ src/repositories/openApiRequest.js
@@ -1,20 +1,33 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const logger = require('../logger');
-const headerFactory = require('../factory/headers')
-const resultHandler = require('../handlers/resultHandler')
-const metrics = require('../metrics/processTimestamp')
+const logger = require("../logger");
+const headerFactory = require("../factory/headers");
+const resultHandler = require("../handlers/resultHandler");
+const metrics = require("../metrics/processTimestamp");
+
+//TODO: test if parametrized IP address works
 
 const getOpenApi = (service) => {
-    logger.info(`querying ${service.metadata.additionalProp1} to get openAPI definition`);
-    const headers = headerFactory.get();
-    const openApiRequest = axios.get(service.metadata.additionalProp1, { headers });
-    return openApiRequest.then(response => {
-        metrics.setOASTimestamp(service.id ,new Date().getTime());
-        return response.data;
-    }).catch(resultHandler.errorHandler);
-}
+  logger.info(
+    `querying http://${arrowHeadService.provider.address}:${arrowHeadService.provider.port}/${
+      service.metadata.additionalProp1.split("/")[3]
+    } to get openAPI definition`
+  );
+  const headers = headerFactory.get();
+  const openApiRequest = axios.get(
+    `http://${arrowHeadService.provider.address}:${arrowHeadService.provider.port}/${
+      service.metadata.additionalProp1.split("/")[3]
+    }`,
+    { headers }
+  );
+  return openApiRequest
+    .then(async (response) => {
+      await metrics.setOASTimestamp(service.id, new Date().getTime());
+      return response.data;
+    })
+    .catch(resultHandler.errorHandler);
+};
 
 module.exports = {
-    getOpenApi
-}
+  getOpenApi,
+};
