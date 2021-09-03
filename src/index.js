6e2cc7660886649cb1e1b863e6293ba663ab2c5b
ivanzy <idimitryz@gmail.com>
2021-09-03 16:24:02 -0300

    fix: rewrote index to fit to translator

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 const logger = require('./logger');
 const server = require('./server');
-const modronRequests = require('./repositories/modronRequests');
-const wotServer = require('./wotServer');
-const pooling = require('./pooling');
 const gConfig = require('./config/conf.json');
 require('dotenv').config()
-
-const getAuth = async () => {
-  if (gConfig.mode.wotRepository === "modron")
-    await modronRequests.tokenRefresher()
-}
 
 server.start()
   .then((res) => {
@@ -17,19 +9,5 @@ server.start()
     logger.info(`WoT-ArrowHead Adapter API up and running`)
   })
   .catch((err) => {
-    logger.error(err);
-  });
-
-getAuth()
-  .then(() => {
-    if (gConfig.mode.wotAdapter)
-      pooling.wotRepository();
-  }).then(() => {
-    if (gConfig.mode.arrowheadAdapter)
-      pooling.arrowHead();
-  })
-  .then(response => {
-    logger.info(response);
-  }).catch(err => {
     logger.error(err);
   });
