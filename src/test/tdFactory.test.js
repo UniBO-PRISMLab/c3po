68877b4c5608109a2415e0aa4bd30ff4d12a39c8
ivanzy <idimitryz@gmail.com>
2021-09-06 20:33:09 -0300

    test: rewrote tests for translation only

--- src/test/tdFactory.test.js
+++ src/test/tdFactory.test.js
@@ -1,22 +1,21 @@
 const tdFactory = require('../factory/tdFactory');
-const openAPIExample1= require('./arrowhead_service_test.json');
-const openAPIExample2= require('./swagger_example.json');
-const openAPIExample3 = require('../api.schema.json')
-const service = require('./service.json')
+const openAPIExample1= require('./mockFiles/arrowhead_service_test.json');
+const openAPIExample2= require('./mockFiles/swagger_example.json');
+const openAPIExample3 = require('./mockFiles/wot_arrowhead_oas.json')
 
 test('converts openAPI to WoT TD', () => {
-  const td1 = tdFactory(service, openAPIExample1);
-  console.info(td1);
+  const td1 = tdFactory(openAPIExample1);
+  //console.info(td1);
   expect(td1).toBeDefined();
   expect(td1).toBeInstanceOf(Object);
   
-  const td2 = tdFactory(service, openAPIExample2);
-  console.info(JSON.stringify(td2));
+  const td2 = tdFactory(openAPIExample2);
+  //console.info(JSON.stringify(td2));
   expect(td2).toBeDefined();
   expect(td2).toBeInstanceOf(Object);
 
-  const td3 = tdFactory(service, openAPIExample3);
-  console.info(JSON.stringify(td3));
+  const td3 = tdFactory(openAPIExample3);
+  //console.info(JSON.stringify(td3));
   expect(td3).toBeDefined();
   expect(td3).toBeInstanceOf(Object);
 });
