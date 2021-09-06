3a3afbfb75392dbae7fba71c07c05cdc6af21f29
ivanzy <idimitryz@gmail.com>
2021-09-06 18:57:17 -0300

    fix(route): fixing / route to reflect to the translator, not the wot-arrowhead adapter

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ require('dotenv').config()
 server.start()
   .then((res) => {
     logger.debug(res);
-    logger.info(`WoT-ArrowHead Adapter API up and running`)
+    logger.info(`Translator up and running`)
   })
   .catch((err) => {
     logger.error(err);
   });
+
+// TODO: 1. user can provide a complete TD with metadata to be translated into;
+// TODO: 2. user can provide a link, to fetch the OAS to be later translated into TD;
+// TODO: In both cases the user can choose to deploy the WT - and where to deploy it to
