4a21d3fb63e4d7308d76aa526607e7a41fe0e65f
ivanzy <idimitryz@gmail.com>
2021-09-06 14:49:08 -0300

    test: test OpenAPI schema validator

--- src/test/isValidOpenAPI.test.js
+++ src/test/isValidOpenAPI.test.js
@@ -1,5 +1,17 @@
 const isValidOAS = require("../validation/validateOAS");
 
 test("validating OpenAPI specification", () => {
+  const validOAS = require("./mockFiles/swagger_example.json");
+  expect(isValidOAS(validOAS)).toBeTruthy();
+
+  const invalidOAS = {
+    name: "invalid",
+    version: 1.0,
+    routes: {
+      firstRoute: "/index",
+      secondRoute: "/client",
+    }
+  };
+  expect(isValidOAS(invalidOAS)).toBeFalsy();
 
 });
