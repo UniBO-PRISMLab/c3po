26f0a232b42319533219a36b3f154a14e2606062
ivanzy <idimitryz@gmail.com>
2021-09-06 20:17:54 -0300

    test: write test to validate OAS from multiple version of the OAS schema

--- src/test/isValidOpenAPI.test.js
+++ src/test/isValidOpenAPI.test.js
@@ -1,17 +1,25 @@
-const isValidOAS = require("../utils/validateOAS");
+const validator = require("../utils/validateOAS");
+
+const openApis = [
+  require("./mockFiles/wot_arrowhead_oas.json"),
+  require("./mockFiles/openapi_gas_sensor.json"),
+  require("./mockFiles/swagger_example.json"),
+];
+
+for (const openApi of openApis)
+  test("validating OpenAPI specification", () => {
+    expect(validator.isValidOAS(openApi)).toBeTruthy();
+  });
+
+const invalidOAS = {
+  name: "invalid",
+  version: 1.0,
+  routes: {
+    firstRoute: "/index",
+    secondRoute: "/client",
+  },
+};
 
 test("validating OpenAPI specification", () => {
-  const validOAS = require("./mockFiles/swagger_example.json");
-  expect(isValidOAS(validOAS)).toBeTruthy();
-
-  const invalidOAS = {
-    name: "invalid",
-    version: 1.0,
-    routes: {
-      firstRoute: "/index",
-      secondRoute: "/client",
-    }
-  };
-  expect(isValidOAS(invalidOAS)).toBeFalsy();
-
+  expect(validator.isValidOAS(invalidOAS)).toBeFalsy();
 });
