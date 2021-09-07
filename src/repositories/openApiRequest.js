62d8a7e1e3294c316e4b5c6817f3ee52d6fc1166
ivanzy <idimitryz@gmail.com>
2021-09-07 20:29:49 -0300

    feat(repositories): rewrite openApiRequest to perform a GET on a url given as a parameter

--- src/repositories/openApiRequest.js
+++ src/repositories/openApiRequest.js
@@ -3,28 +3,13 @@ const axios = require("axios");
 const logger = require("../logger");
 const headerFactory = require("../factory/headers");
 const resultHandler = require("../handlers/resultHandler");
-const metrics = require("../metrics/processTimestamp");
 
-//TODO: test if parametrized IP address works
-
-const getOpenApi = (service) => {
-  logger.info(
-    `querying http://${arrowHeadService.provider.address}:${arrowHeadService.provider.port}/${
-      service.metadata.additionalProp1.split("/")[3]
-    } to get openAPI definition`
-  );
+const getOpenApi = (url) => {
+  logger.info(`querying ${url} to get openAPI definition`);
   const headers = headerFactory.get();
-  const openApiRequest = axios.get(
-    `http://${arrowHeadService.provider.address}:${arrowHeadService.provider.port}/${
-      service.metadata.additionalProp1.split("/")[3]
-    }`,
-    { headers }
-  );
+  const openApiRequest = axios.get(url, { headers });
   return openApiRequest
-    .then(async (response) => {
-      await metrics.setOASTimestamp(service.id, new Date().getTime());
-      return response.data;
-    })
+    .then(async (response) => response.data)
     .catch(resultHandler.errorHandler);
 };
 
