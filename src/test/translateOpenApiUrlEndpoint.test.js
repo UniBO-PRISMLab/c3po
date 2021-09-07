5f1bd0b30364e524e2d50fb9efd264e5a9c02478
ivanzy <idimitryz@gmail.com>
2021-09-07 20:32:09 -0300

    test: testing endpoint /translateOpenApi/url

---
+++ src/test/translateOpenApiUrlEndpoint.test.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+
+const gConfig = require("../config/conf.json");
+
+test("POST /translateOpenApi/url", async () => {
+  const host = gConfig.translator.host;
+  const port = gConfig.translator.port || 3000;
+  const openApisUrls = [
+    "https://petstore.swagger.io/v2/swagger.json",
+    "https://raw.githubusercontent.com/adafruit/io-api/gh-pages/v2.json",
+  ];
+
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  for (const openApisUrl of openApisUrls) {
+    const response = await axios.post(
+      `http://${host}:${port}/translateOpenApi/url`,
+      { url: openApisUrl },
+      { headers }
+    );
+
+    try {
+      expect(response.status).toBe(200);
+      expect(response.data).toBeDefined();
+    } catch (error) {
+      error.message = `${error.message}
+      \n\nresponse status: ${response.status}`;
+      throw error;
+    }
+  }
+});
