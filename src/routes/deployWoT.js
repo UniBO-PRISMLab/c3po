d355a47d54d9679e6191df57c7e274b0d96c1c52
ivanzy <idimitryz@gmail.com>
2021-09-09 18:54:59 -0300

    feat: translation of a OpenAPI schema and the deployment of the resuting TD as a Web Thing

---
+++ src/routes/deployWoT.js
@@ -0,0 +1,40 @@
+//TODO: add authentication
+const logger = require("../logger");
+const validUrl = require("valid-url");
+
+const deployOpenApi = require("../utils/deployOpenApi");
+const openApiRequest = require("../repositories/request");
+
+module.exports = (router) => {
+  //user send the openAPI schema in the body of the request as a JSON
+  router.route("/").post((req, res) => {
+    logger.info("received a POST request at /deployWoT");
+    logger.debug(req.body);
+
+    const openApi = req.body;
+    deployOpenApi(openApi, (status, response) => {
+      res.status(status).send(response);
+    });
+  });
+  //user sends the URL (as {"url": "someurl.com" }) that exposes a OpenAPI schema in JSON
+  router.route("/url").post(async (req, res) => {
+    logger.info("received a POST request at /deployWoT/url");
+    logger.debug(req.body);
+
+    const url = req.body.url;
+    if (validUrl.isUri(url)) {
+      try {
+        const openApi = await openApiRequest.getUrl(url);
+        deployOpenApi(openApi, (status, response) => {
+          res.status(status).send(response);
+        });
+      } catch (err) {
+        res.status(404).send(`Error fetching OpenAPI request
+          \n error message: ${err}`);
+      }
+    } else {
+      logger.info(`Invalid URL (${url}) received at /translateOpenApi/url`);
+      res.status(400).send(`Received an invalid URL (${url})`);
+    }
+  });
+};
