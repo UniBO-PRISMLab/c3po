285ddc39c7bd3e9d4deb6a480604c578398e20c8
ivanzy <idimitryz@gmail.com>
2021-09-09 18:43:44 -0300

    style(repositories): renaming OpenAPI request to only requests
    The function does not have any specific OpenAPI code, it just performs simple get request

--- src/repositories/request.js
+++ src/repositories/request.js
@@ -4,15 +4,15 @@ const logger = require("../logger");
 const headerFactory = require("../factory/headers");
 const resultHandler = require("../handlers/resultHandler");
 
-const getOpenApi = (url) => {
-  logger.info(`querying ${url} to get openAPI definition`);
+const getUrl = (url) => {
+  logger.info(`Querying ${url}`);
   const headers = headerFactory.get();
-  const openApiRequest = axios.get(url, { headers });
-  return openApiRequest
+  const response = axios.get(url, { headers });
+  return response
     .then(async (response) => response.data)
     .catch(resultHandler.errorHandler);
 };
 
 module.exports = {
-  getOpenApi,
+  getUrl,
 };
