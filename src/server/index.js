af30549a3f5767148e025f6bb5a1b704254658f6
ivan <idimitryz@gmail.com>
2021-11-30 15:43:45 +0100

    feat: add cors origin:*

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
-const swaggerUi = require('swagger-ui-express');
-const OpenApiValidator = require('express-openapi-validator')
+const swaggerUi = require("swagger-ui-express");
+const OpenApiValidator = require("express-openapi-validator");
 
-const gConfig = require("../config/conf.json")
-const apiSchema = require("../config/swagger/api.schema.json")
+const gConfig = require("../config/conf.json");
+const apiSchema = require("../config/swagger/api.schema.json");
 
 const routesPath = "../routes";
 
@@ -14,27 +15,36 @@ const port = gConfig.translator.port || 3000;
 const app = express();
 
 const docsSetup = async () => {
-    const options = {
-        swaggerOptions: {
-            url: `http://${host}:${port}/openapi`
-        }
-    };
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options));
-    app.use(
-        OpenApiValidator.middleware({
-            apiSpec: apiSchema,
-            validateRequests: true,
-            validateResponses: true,
-        })
-    );
-}
+  const options = {
+    swaggerOptions: {
+      url: `http://${host}:${port}/openapi`,
+    },
+  };
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(null, options)
+  );
+  app.use(
+    OpenApiValidator.middleware({
+      apiSpec: apiSchema,
+      validateRequests: true,
+      validateResponses: true,
+    })
+  );
+};
 
 module.exports.start = async () => {
-    app.use(express.json());
-    //Initialize routes
-    await require(routesPath)(app);
-    //setup swagger UI
-    await docsSetup();
-    //run server
-    return await app.listen(port);
+  app.use(
+    cors({
+      origin: "*",
+    })
+  );
+  app.use(express.json());
+  //Initialize routes
+  await require(routesPath)(app);
+  //setup swagger UI
+  await docsSetup();
+  //run server
+  return await app.listen(port);
 };
