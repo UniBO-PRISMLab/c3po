de872e60912b2357ca729ddc451cd05609ae5887
ivan <idimitryz@gmail.com>
2022-01-12 16:57:44 +0100

    fix: added new conf for each server (http/coap)

--- src/utils/generateWoTUrl.js
+++ src/utils/generateWoTUrl.js
@@ -1,6 +1,6 @@
 const gConfig = require("../config/conf.json");
 
 const generateWoTUrl = (response) =>
-  `http://${gConfig.wot.host}:${gConfig.wot.port}/${response.title}`;
+  `http://${gConfig.wot.http.host}:${gConfig.wot.http.port}/${response.title}`;
 
   module.exports = generateWoTUrl;
