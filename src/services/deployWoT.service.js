e0cfe86e3ee51089d48199506612c840e745eea7
ivan <idimitryz@gmail.com>
2022-01-12 17:04:00 +0100

    feat: logging user and other request information into a file

--- src/services/deployWoT.service.js
+++ src/services/deployWoT.service.js
@@ -8,149 +8,159 @@ const resultHandler = require("../utils/handlers/resultHandler");
 const headerFactory = require("../utils/headers");
 const replacer = require("../utils/replacer");
 
-//TODO: refactor this function. There is a lot of code duplication
 const createThing = async (td, serviceUrl) => {
   if (typeof wot.getServer() === "undefined")
     await wot.startServer();
   const thing = await wot.getServer().produce(td);
 
-  for (const key in thing.properties) {
+  for (const propertyName in thing.properties) {
     logger.info(
-      `setting ${key} property handler of ${
+      `setting ${propertyName} property handler of ${
         td.title
-      } Thing to ${serviceUrl}/${replacer.formatUri(key)}`
+      } Thing to ${serviceUrl}/${replacer.formatUri(
+        propertyName
+      )}`
     );
 
-    if (key.includes("put-")) {
-      thing.setPropertyWriteHandler(key, async (input) => {
-        const headers = headerFactory.put();
-        const propertyRequest = axios.put(
-          `${serviceUrl}/${replacer.formatUri(key)}`,
-          input,
-          { headers }
-        );
-        logger.info(
-          `performing PUT request for ${key} property of ${
-            td.title
-          }  at ${`${serviceUrl}/${replacer.formatUri(
-            key
-          )}`}`
-        );
-        return propertyRequest
-          .then((response) => {
-            logger.info("PUT response:");
-            console.log(response.data);
-            response.data;
-          })
-          .catch(resultHandler.errorHandler);
-      });
+    if (propertyName.includes("put-"))
+      thing.setPropertyWriteHandler(
+        propertyName,
+        async (input, options = false) =>
+          setAffordance(
+            "put",
+            serviceUrl,
+            td.title,
+            propertyName,
+            options,
+            input
+          )
+      );
+
+    if (propertyName.includes("patch-")) {
+      thing.setPropertyWriteHandler(
+        propertyName,
+        async (input, options = false) =>
+          setAffordance(
+            "patch",
+            serviceUrl,
+            td.title,
+            propertyName,
+            options,
+            input
+          )
+      );
     }
-    if (key.includes("patch-")) {
-      thing.setPropertyWriteHandler(key, async (input) => {
-        const headers = headerFactory.patch();
-        const propertyRequest = axios.patch(
-          `${serviceUrl}/${replacer.formatUri(key)}`,
-          input,
-          { headers }
+    if (propertyName.includes("get-"))
+      thing.setPropertyReadHandler(
+        propertyName,
+        async (options = false) =>
+          setAffordance(
+            "get",
+            serviceUrl,
+            td.title,
+            propertyName,
+            options
+          )
+      );
+
+    for (const actionName in thing.actions) {
+      if (actionName.includes("delete-"))
+        thing.setActionHandler(
+          actionName,
+          async (input = false, options) =>
+            setAffordance(
+              "delete",
+              serviceUrl,
+              td.title,
+              actionName,
+              options,
+              input
+            )
         );
-        logger.info(
-          `performing PATCH request for ${key} property of ${
-            td.title
-          }  at ${`${serviceUrl}/${replacer.formatUri(
-            key
-          )}`}`
+      if (actionName.includes("post-"))
+        thing.setActionHandler(
+          actionName,
+          async (input, options) =>
+            setAffordance(
+              "post",
+              serviceUrl,
+              td.title,
+              actionName,
+              options,
+              input
+            )
         );
-        return propertyRequest
-          .then((response) => {
-            logger.info("PATCH response:");
-            console.log(response.data);
-            response.data;
-          })
-          .catch(resultHandler.errorHandler);
-      });
     }
-    if (key.includes("get-"))
-      thing.setPropertyReadHandler(key, async () => {
-        const headers = headerFactory.get();
-        const propertyRequest = axios.get(
-          `${serviceUrl}/${replacer.formatUri(key)}`,
-          { headers }
-        );
-        logger.info(
-          `performing GET request for ${key} property of ${
-            td.title
-          } Thing at ${`${serviceUrl}/${replacer.formatUri(
-            key
-          )}`}`
-        );
-        return propertyRequest
-          .then((response) => response.data)
-          .catch(resultHandler.errorHandler);
-      });
+
+    await thing.expose();
+
+    logger.info(
+      `Web Thing ${
+        thing.getThingDescription().title
+      } exposed at ${gConfig.wot.http.port}/${
+        thing.getThingDescription().title
+      }`
+    );
   }
-
-  for (const key in thing.actions)
-    setAction(thing, key, serviceUrl, td.title);
-
-  await thing.expose();
-
-  logger.info(
-    `Web Thing ${
-      thing.getThingDescription().title
-    } exposed at ${gConfig.wot.port}/${
-      thing.getThingDescription().title
-    }`
-  );
 };
 
-const setAction = (
-  thing,
-  actionName,
+const setAffordance = (
+  method,
   serviceUrl,
-  title
+  title,
+  key,
+  options,
+  input = false
 ) => {
-  thing.setActionHandler(
-    actionName,
-    async (input = false) => {
-      const url = `${serviceUrl}/${replacer.formatUri(
-        actionName
-      )}`;
-
-      const headers = headerFactory.post();
-      if (actionName.includes("delete-")) {
-        const request = { headers: headers };
-        if (input) request.data = input;
-        const actionDeleteRequest = axios.delete(
-          url,
-          request
-        );
-        logger.info(
-          `performing ${actionName} action of ${title} thing to ${url} - input: ${JSON.stringify(
-            input
-          )}`
-        );
-        return actionDeleteRequest
-          .then((response) => response.data)
-          .catch(resultHandler.errorHandler);
-      } else if (actionName.includes("post-")) {
-        logger.info(url);
-        const actionPostRequest = axios.post(url, input, {
-          headers,
-        });
-        logger.info(
-          `performing ${actionName} action of ${title} thing to ${url} - input: ${JSON.stringify(
-            input
-          )}`
-        );
-        return actionPostRequest
-          .then((response) => {
-            logger.info(response.data);
-            return response.data;
-          })
-          .catch(resultHandler.errorHandler);
-      }
-    }
+  const uri = replacer.formatUri(key);
+  const propertyRequest = setRequest[method](
+    `${serviceUrl}/${uri}`,
+    input
   );
+
+  logger.network({
+    thing: title,
+    operation: method.toUpperCase(),
+    uri: uri,
+    user: `${options.data.host}:${options.data.port}`,
+  });
+
+  logger.info(
+    `performing ${method.toUpperCase()} request for ${key} property of ${title}  at ${`${serviceUrl}/${uri}`}`
+  );
+  return propertyRequest
+    .then((response) => {
+      logger.info({
+        msg: `${method.toUpperCase()} response`,
+        response: response.data,
+      });
+      return response.data;
+    })
+    .catch(resultHandler.errorHandler);
+};
+
+const setRequest = {
+  get: (url) =>
+    axios.get(url, { headers: headerFactory.get() }),
+  put: (url, input) =>
+    axios.put(url, input, { headers: headerFactory.put() }),
+  patch: (url, input) =>
+    axios.patch(url, input, {
+      headers: headerFactory.patch(),
+    }),
+  post: (url, input) =>
+    axios.post(url, input, {
+      headers: headerFactory.post(),
+    }),
+  delete: (url, input) =>
+    input
+      ? axios.delete(url, {
+          headers: headerFactory.delete(),
+          data: input,
+        })
+      : axios.delete(url, {
+          headers: headerFactory.delete(),
+        }),
 };
 
 module.exports = createThing;
