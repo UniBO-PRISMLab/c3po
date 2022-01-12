6681a0d7cbe3b8c3c59fdbb3a4ec6408a0d8866e
ivan <idimitryz@gmail.com>
2022-01-12 16:56:42 +0100

    feat: added coap wot binding

--- src/server/wot.server.js
+++ src/server/wot.server.js
@@ -1,18 +1,32 @@
 const Servient = require("@node-wot/core").Servient;
-const HttpServer = require("@node-wot/binding-http").HttpServer
+const HttpServer =
+  require("@node-wot/binding-http").HttpServer;
+/* const MqttBrokerServer =
+  require("@node-wot/binding-mqtt").MqttBrokerServer; */
+const CoapServer =
+  require("@node-wot/binding-coap").CoapServer;
 
-const gConfig = require('../config/conf.json')
+// create Servient add MQTT binding
+const gConfig = require("../config/conf.json");
 
 const wot = {
-    "wotServer": undefined,
-    async startServer() {
-        const servient = new Servient();
-        servient.addServer(new HttpServer({ port: gConfig.wot.port }));
-        this.wotServer = await servient.start();
-    },
-    getServer() {
-        return this.wotServer;
-    }
-}
+  wotServer: undefined,
+  async startServer() {
+    const servient = new Servient();
+    servient.addServer(
+      new HttpServer({ port: gConfig.wot.http.port })
+    );
+    servient.addServer(new CoapServer());
+/*     servient.addServer(
+      new MqttBrokerServer(
+        `mqtt://${gConfig.wot.mqtt.broker}:${gConfig.wot.mqtt.port}`
+      )
+    ); */
+    this.wotServer = await servient.start();
+  },
+  getServer() {
+    return this.wotServer;
+  },
+};
 
 module.exports = wot;
