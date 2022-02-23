c84017ba069944e97559116157b5e84afab507cf
ivan <idimitryz@gmail>
2022-02-23 09:26:07 +0000

    fix: corrected the url generation bug

--- src/utils/generateWoTUrl.js
+++ src/utils/generateWoTUrl.js
@@ -1,6 +1,10 @@
 const gConfig = require("../config/conf.json");
 
-const generateWoTUrl = (response) =>
-  `http://${gConfig.wot.http.host}:${gConfig.wot.http.port}/${response.title}`;
+const generateWoTUrl = (response) => {
+  //console.log(response);
+  return `http://${gConfig.wot.http.host}:${
+    gConfig.wot.http.port
+  }/${response.title.replace(/ /g, "-")}`;
+};
 
-  module.exports = generateWoTUrl;
+module.exports = generateWoTUrl;
